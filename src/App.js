821638d1b4af33217939c4df2c6e3cb771841b0b
tahmit <tahmit@uw.edu>
2020-02-19 18:05:36 -0800

    writing async function to fetch from api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  async function getMons() {
+    let url = 'https://api.giphy.com/v1/gifs/search?'
+    url += 'api_key='+key
+    url += '&q='+text
+    const r = await fetch(url)
+    const body = await r.json()
+  }
+
   return (
     <div className="App">
       <PokeList/>
