2153e96d020ba4b412f815e14652c55e663a4fad
tahmit <tahmit@uw.edu>
2020-02-19 18:04:56 -0800

    Added CSS for poke list component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PokeList/>
     </div>
+  );
+}
+
+function PokeList() {
+  return (<div className="poke-list">
+
+  </div>
   );
 }
 
