8e963278bf7144474a843610387401d0d5b71375
tahmit <tahmit@uw.edu>
2020-02-19 17:32:50 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
