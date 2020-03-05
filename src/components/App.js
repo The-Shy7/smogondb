7fa06fce4b11515fb6f4d4f10c0c054c1107d239
tahmit <tahmit@uw.edu>
2020-03-04 21:13:46 -0800

    Finished adding missing components

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,27 @@
-import React, {useState} from "react";
+import React, { Component,useState } from "react";
+import PokeCalls from "./pokecalls/PokeCalls";
 import SearchBar from "./searchBar/SearchBar";
 import Detail from "./detail/Detail";
+import NavBar from "./navbar/NavBar";
+import Generations from "./generations/Generations";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ErrorMessage from "./errorMessage/ErrorMessage";
 
 function App() {
   const [typeFilters, setTypeFilters] = useState([])
 
-  handleFilters = (typeFilters) => {
+  handleFilters = ({typeFilters}) => {
     if (!typeFilters) {
-      setTypeFilters = []
+      setTypeFilters([])
     } 
-    
-    setTypeFilters = typeFilters
+
+    setTypeFilters(typeFilters)
   }
 
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div>
+        <NavBar />
 
         <SearchBar filter={this.handleFilters} />
         <Switch>
@@ -27,6 +31,7 @@ function App() {
             render={() => <PokeCalls filterList={this.state.typeFilters} />}
           />
           <Route exact path="/detail/:name" component={Detail} />
+          <Route exact path="/generations/:num" component={Generations} />
           <Route component={ErrorMessage} />
         </Switch>
       </div>
