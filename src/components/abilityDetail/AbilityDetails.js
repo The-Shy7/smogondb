443daa992371f61e24463382d2543ba7d100ef4e
tahmit <tahmit@uw.edu>
2020-03-04 21:14:25 -0800

    Renders info on Pokemon's abilities

---
+++ src/components/abilityDetail/AbilityDetails.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import DetailsCalls from "../detail/DetailsCalls";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import KeyboardArrowDown from "@material-ui/icons/KeyboardArrowDown";
+
+const AbilityDetails = ability => {
+  const [showAbility, setShowAbility] = useState(false);
+  return (
+    <div className="ability">
+      <span key={ability.name} className="s-Ability">
+        {" "}
+        {ability.name}{" "}
+      </span>
+      <button
+        className="moveButton"
+        onClick={() => setShowAbility(!showAbility)}
+      >
+        {showAbility ? (
+          <KeyboardArrowDown style={{ fontSize: 17 }} />
+        ) : (
+          <KeyboardArrowRight style={{ fontSize: 17 }} />
+        )}
+      </button>
+      <div className="callsContainer">
+        {showAbility ? <DetailsCalls abilityUrl={ability.url} /> : ""}
+      </div>
+    </div>
+  )
+}
+
+export default AbilityDetails;
