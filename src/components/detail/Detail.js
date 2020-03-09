b65be83f010e0c289570fe84a9e4781f2c1189c0
tahmit <tahmit@uw.edu>
2020-03-08 22:47:36 -0700

    Changed to constructor-render

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -1,188 +1,202 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import "./Detail.css";
 import AbilityDetail from "../abilityDetail/AbilityDetails";
 import Loading from "./../loading/Loading";
 import ErrorMessage from "./../errorMessage/ErrorMessage";
 
-function Detail() {
-    const [pokeData, setPokeData] = useState([])
-    const [isLoaded, setIsLoaded] = useState(false)
-    const [abilityOn, setAbilityOn] = useState(false)
-    const [abilityUrl, setAbilityUrl] = useState('')
-    const [weight, setWeight] = useState('')
-    const [moveUrl, setMoveUrl] = useState('')
-    const [key, setKey] = useState('')
-    const [error, setError] = useState(false)
-    const [errorCode, setErrorCode] = useState(null)
+class Detail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokeData: [],
+      isLoaded: false,
+      abilityOn: false,
+      abilityUrl: "",
+      weight: "",
+      moveUrl: "",
+      key: "",
+      error: false,
+      errorCode: null
+    };
+  }
+  componentDidMount = () => {
+    this.fetchPokeName();
+  };
 
-    componentDidFetch = () => {
-        this.fetchPokeName()
+  componentDidUpdate = (prevState, prevProps) => {
+    if (this.props.location.pathname !== prevState.location.pathname) {
+      this.fetchPokeName();
     }
+  };
+  fetchPokeName = async () => {
+    this.setState({
+      isLoaded: false,
+      error: false
+    });
+    const pokeName = this.props.match.params.name;
+    const lowerCasePokeName = pokeName.toLowerCase();
+    try {
+      const res = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${lowerCasePokeName}/`
+      );
+      this.setState(
+        {
+          pokeData: res.data,
+          isLoaded: true
+        },
+        this.handleWeightChange(res.data.weight)
+      );
+    } catch (error) {
+      this.setState({
+        errorCode: error.request.status,
+        error: true,
+        isLoaded: true
+      });
+    }
+  };
+  handleWeightChange = weight => {
+    //Fixes weight to better show weight
+    let weight1 = weight.toString();
+    let newWeight = weight1.slice(0, -1) + "." + weight1.slice(-1);
+    this.setState({ weight: newWeight });
+  };
+  handleChange = (name, url, key) => {
+    if (name === "ability") {
+      this.setState({
+        abilityUrl: url,
+        abilityOn: !this.state.abilityOn,
+        key: key
+      });
+    } else {
+      this.setState({ key: "" });
+    }
+  };
+  renderType = params => {
+    switch (params) {
+      default:
+        return "";
+      case "grass":
+        return "grass";
+      case "poison":
+        return "poison";
+      case "fire":
+        return "fire";
+      case "water":
+        return "water";
+      case "fighting":
+        return "fighting";
+      case "flying":
+        return "flying";
+      case "dragon":
+        return "dragon";
+      case "normal":
+        return "normal";
+      case "fairy":
+        return "fairy";
+      case "electric":
+        return "electric";
+      case "ground":
+        return "ground";
+      case "ice":
+        return "ice";
+      case "dark":
+        return "dark";
+      case "steel":
+        return "steel";
+      case "bug":
+        return "bug";
+      case "psychic":
+        return "psychic";
+      case "rock":
+        return "rock";
+      case "ghost":
+        return "ghost";
+    }
+  };
 
-    componentDidUpdate = (prevState, prevProps) => {
-        if (this.props.location.pathname !== prevState.location.pathname) {
-            this.fetchPokeName()
-        }
-    }
-
-    async function fetchPokeName() {
-        setIsLoaded(false)
-        setError(false)
-        const pokeName = this.props.match.params.name
-        const lowerCasePokeName = pokeName.toLowerCase();
-        try {
-            const r = await axios.get(`https://pokeapi.co/api/v2/pokemon/${lowerCasePokeName}/`)
-            setPokeData(r.data)
-            setIsLoaded(true)
-            this.handleWeightChange(res.data.weight)
-        } catch (error) {
-            setErrorCode(error.request.status)
-            setError(true)
-            setIsLoaded(true)
-        }
-    }
-
-    handleWeightChange = (weight) => {
-        let temp = weight.toString()
-        let newWeight = temp.slice(0,-1) + "." + weight1.slice(-1)
-        setWeight(newWeight)
-    }
-
-    handleChange = (name, url, key) => {
-        if (name === "ability") {
-            setAbilityUrl(url)
-            setAbilityOn(!{abilityOn})
-            setKey(key)
-        } else {
-            setKey('')
-        }
-    }
-
-    renderType = (params) => {
-        switch (params) {
-            default:
-                return "";
-            case "grass":
-                return "grass";
-            case "poison":
-                return "poison";
-            case "fire":
-                return "fire";
-            case "water":
-                return "water";
-            case "fighting":
-                return "fighting";
-            case "flying":
-                return "flying";
-            case "dragon":
-                return "dragon";
-            case "normal":
-                return "normal";
-            case "fairy":
-                return "fairy";
-            case "electric":
-                return "electric";
-            case "ground":
-                return "ground";
-            case "ice":
-                return "ice";
-            case "dark":
-                return "dark";
-            case "steel":
-                return "steel";
-            case "bug":
-                return "bug";
-            case "psychic":
-                return "psychic";
-            case "rock":
-                return "rock";
-            case "ghost":
-                return "ghost";
-        }   
-    }
+  render() {
+    const { pokeData, isLoaded, weight, error, errorCode } = this.state;
 
     if (!isLoaded) {
-        return(
-            <div className="loadingContainer">
-                <Loading />
-            </div>
-        );
+      return (
+        <div className="loadingContainer">
+          <Loading />
+        </div>
+      );
     }
-
     if (error) {
-        return(
-            <div className="detailError">
-                <ErrorMessage
-                    errorCode={errorCode}
-                    pokeNames={this.props.match.params.name}
-                />
-            </div> 
-        );
+      return (
+        <div className="detailError">
+          <ErrorMessage
+            errorCode={errorCode}
+            pokeNames={this.props.match.params.name}
+          />
+        </div>
+      );
     }
-
     if (isLoaded) {
-        return(
-            <div className="pokeDetail">
-            <div className="topBoxWrapper">
-              <div className="pokePicture">
-                <div className="pokeNameContainer">
-                  <h3 className="pokeDetailName">{pokeData.name}</h3>
-                  <p>#{pokeData.id}</p>
-                </div>
-                <img src={pokeData.sprites.front_default} alt="pokemon icon" />
+      return (
+        <div className="pokeDetail">
+          <div className="topBoxWrapper">
+            <div className="pokePicture">
+              <div className="pokeNameContainer">
+                <h3 className="pokeDetailName">{pokeData.name}</h3>
+                <p>#{pokeData.id}</p>
               </div>
-              <div className="weightContainer">
-                <p className="p-weight"> Weight </p>
-                <div className="weight">
-                  <p> {weight} kg </p>
-                </div>
-              </div>
-              <div className="typeContainer">
-                {pokeData.types.map(({ type }, i) => (
-                  <div className={this.renderType(type.name)}>
-                    <span className="s-Type" key={i}>
-                      {type.name}
-                    </span>
-                  </div>
-                ))}
-              </div>
-              <div className="shiny">
-                <p className="p-Shiny">Shiny Version </p>
-                <div className="shinySprite">
-                  <img src={pokeData.sprites.front_shiny} alt="shiny version" />
-                </div>
+              <img src={pokeData.sprites.front_default} alt="pokemon icon" />
+            </div>
+            <div className="weightContainer">
+              <p className="p-weight"> Weight </p>
+              <div className="weight">
+                <p> {weight} kg </p>
               </div>
             </div>
-            <div className="abilityContainer">
-              <h3 className="p-Abilites">Abilities</h3>
-              <div className="pokeAbilities">
-                {pokeData.abilities.map(({ ability }, i) => (
-                  <AbilityDetail key={i} name={ability.name} url={ability.url} />
-                ))}
-              </div>
+            <div className="typeContainer">
+              {pokeData.types.map(({ type }, i) => (
+                <div className={this.renderType(type.name)}>
+                  <span className="s-Type" key={i}>
+                    {type.name}
+                  </span>
+                </div>
+              ))}
             </div>
-            <div className="stats">
-              <h3 className="p-Stats"> Stats </h3>
-              <div className="pokeContainer">
-                {pokeData.stats.map(({ base_stat, stat }, i) => (
-                  <div>
-                    <span className="statName" key={i}>
-                      {" "}
-                      {stat.name}{" "}
-                    </span>
-                    <div className="statContainer">
-                      <div style={{ width: base_stat }} className="statBar">
-                        <span>{base_stat}</span>
-                      </div>
-                    </div>
-                  </div>
-                ))}
+            <div className="shiny">
+              <p className="p-Shiny">Shiny Version </p>
+              <div className="shinySprite">
+                <img src={pokeData.sprites.front_shiny} alt="shiny version" />
               </div>
             </div>
           </div>
-        );
+          <div className="abilityContainer">
+            <h3 className="p-Abilites">Abilities</h3>
+            <div className="pokeAbilities">
+              {pokeData.abilities.map(({ ability }, i) => (
+                <AbilityDetail key={i} name={ability.name} url={ability.url} />
+              ))}
+            </div>
+          </div>
+          <div className="stats">
+            <h3 className="p-Stats"> Stats </h3>
+            <div className="pokeContainer">
+              {pokeData.stats.map(({ base_stat, stat }, i) => (
+                <div>
+                  <span className="statName" key={i}>
+                    {" "}
+                    {stat.name}{" "}
+                  </span>
+                  <div className="statContainer">
+                    <div style={{ width: base_stat }} className="statBar">
+                      <span>{base_stat}</span>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      );
     }
+  }
 }
-
 export default Detail;
--- src/components/detail/DetailCalls.js
+++ src/components/detail/DetailCalls.js
@@ -1,128 +1,145 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import axios from "axios";
-import "./DetailsCalls.css";
+import "./DetailCalls.css";
 import Loading from "./../loading/Loading";
 
-function DetailsCalls() {
-    const [abilitiesList, setAbilitiesList] = useState([])
-    const [movesList, setMovesList] = useState([])
-    const [isLoaded, setIsLoaded] = useState(false)
-    const [effectChance, setEffectChance] = useState('')
-
-    function componentDidFetch() {
-        if (this.props.abilityUrl) {
-            setIsLoaded(false)
-            axios,get(this.props.abilityUrl).then(all => {
-                setAbilitiesList(all.data)
-                setIsLoaded(true)
-            })
-        } else if (this.props.moveUrl) {
-            setIsLoaded(false)
-            axios.get(this.props.moveUrl).then(all => {
-                setMovesList(all.data)
-                setIsLoaded(true)
-                this.effectSort
-            })
-        }
+export default class DetailCalls extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      abilitesList: [],
+      movesList: [],
+      isLoaded: false,
+      effectChance: ""
+    };
+  }
+  componentDidMount() {
+    if (this.props.abilityUrl) {
+      this.setState({ isLoaded: false });
+      axios.get(this.props.abilityUrl).then(all => {
+        this.setState({
+          abilitesList: all.data,
+          isLoaded: true
+        });
+      });
+    } else if (this.props.moveUrl) {
+      this.setState({ isLoaded: false });
+      axios.get(this.props.moveUrl).then(all => {
+        this.setState(
+          {
+            movesList: all.data,
+            isLoaded: true
+          },
+          this.effectSort
+        );
+      });
     }
-
-    effectSort = () => {
-        const effect = {movesList}.effect_entries;
-        if ({movesList}.effect_chance) {
-            let newEffect = effect[0].short_effect.slice(0, 6) + movesList.effect_chance + effect[0].short_effect.slice(20)
-            setEffectChance(newEffect)
-        }
+  }
+  effectSort = () => {
+    //Adds percentage value to effect chance
+    const { movesList } = this.state;
+    const effect = movesList.effect_entries;
+    if (movesList.effect_chance) {
+      let newEffect =
+        effect[0].short_effect.slice(0, 6) +
+        movesList.effect_chance +
+        effect[0].short_effect.slice(20);
+      this.setState({ effectChance: newEffect });
     }
+  };
+  renderTypes = params => {
+    switch (params) {
+      default:
+        return "";
+      case "grass":
+        return "grassDesc";
+      case "poison":
+        return "poisonDesc";
+      case "fire":
+        return "fireDesc";
+      case "water":
+        return "waterDesc";
+      case "fighting":
+        return "fightingDesc";
+      case "flying":
+        return "flyingDesc";
+      case "dragon":
+        return "dragonDesc";
+      case "normal":
+        return "normalDesc";
+      case "fairy":
+        return "fairyDesc";
+      case "electric":
+        return "electricDesc";
+      case "ground":
+        return "groundDesc";
+      case "ice":
+        return "iceDesc";
+      case "dark":
+        return "darkDesc";
+      case "steel":
+        return "steelDesc";
+      case "bug":
+        return "bugDesc";
+      case "psychic":
+        return "psychicDesc";
+      case "rock":
+        return "rockDesc";
 
-    renderType = (params) => {
-        switch (params) {
-            default:
-                return "";
-            case "grass":
-                return "grass";
-            case "poison":
-                return "poison";
-            case "fire":
-                return "fire";
-            case "water":
-                return "water";
-            case "fighting":
-                return "fighting";
-            case "flying":
-                return "flying";
-            case "dragon":
-                return "dragon";
-            case "normal":
-                return "normal";
-            case "fairy":
-                return "fairy";
-            case "electric":
-                return "electric";
-            case "ground":
-                return "ground";
-            case "ice":
-                return "ice";
-            case "dark":
-                return "dark";
-            case "steel":
-                return "steel";
-            case "bug":
-                return "bug";
-            case "psychic":
-                return "psychic";
-            case "rock":
-                return "rock";
-            case "ghost":
-                return "ghost";
-        }   
+      case "ghost":
+        return "ghostDesc";
     }
+  };
+
+  render() {
+    const { abilitesList, movesList, isLoaded } = this.state;
 
     if (!isLoaded) {
-        return <Loading />
+      return <Loading />;
     }
-
     if (this.props.abilityUrl) {
-        return (
-            <div className="abilityDesc">
-              {abilitesList.effect_entries.map(({ short_effect }, i) => (
-                <span key={i}>{short_effect}</span>
-              ))}
-            </div>
-        );
+      return (
+        <div className="abilityDesc">
+          {abilitesList.effect_entries.map(({ short_effect }, i) => (
+            <span key={i}>{short_effect}</span>
+          ))}
+        </div>
+      );
     } else if (this.props.moveUrl) {
-        return (
-            <div className="moveDesc">
-              <div className="Accuracy">
-                {movesList.accuracy ? (
-                  <span> Accuracy: {movesList.accuracy}%</span>
-                ) : (
-                  <span> Accuracy: N/A </span>
-                )}
-              </div>
-              <div className="PP">
-                <span> PP: {movesList.pp}</span>
-              </div>
-              <div className="Power">
-                {movesList.power ? (
-                  <span> Power: {movesList.power}</span>
-                ) : (
-                  <span> Power: N/A </span>
-                )}
-              </div>
-              <div className="moveType">
-                <span className="typeText"> Type: </span>
-                <span className={this.renderTypes(movesList.type.name)}>
-                  {movesList.type.name}{" "}
-                </span>
-              </div>
-              <div className="moveEffect">
-                {this.state.effectChance ? (
-                  <span> {this.state.effectChance} </span>
-                ) : (
-                  <span>{movesList.effect_entries[0].short_effect} </span>
-                )}
-              </div>
-            </div>
-        );
+      return (
+        <div className="moveDesc">
+          <div className="Accuracy">
+            {movesList.accuracy ? (
+              <span> Accuracy: {movesList.accuracy}%</span>
+            ) : (
+              <span> Accuracy: N/A </span>
+            )}
+          </div>
+          <div className="PP">
+            <span> PP: {movesList.pp}</span>
+          </div>
+          <div className="Power">
+            {movesList.power ? (
+              <span> Power: {movesList.power}</span>
+            ) : (
+              <span> Power: N/A </span>
+            )}
+          </div>
+          <div className="moveType">
+            <span className="typeText"> Type: </span>
+            <span className={this.renderTypes(movesList.type.name)}>
+              {movesList.type.name}{" "}
+            </span>
+          </div>
+          <div className="moveEffect">
+            {this.state.effectChance ? (
+              <span> {this.state.effectChance} </span>
+            ) : (
+              <span>{movesList.effect_entries[0].short_effect} </span>
+            )}
+          </div>
+        </div>
+      );
     }
+  }
 }
