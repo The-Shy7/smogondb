84f5f6c3052b4e3f22c0a79760a46f60d1b4e00e
tahmit <tahmit@uw.edu>
2020-03-08 22:53:08 -0700

    Need to write rest of component

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import MenuIcon from "@material-ui/icons/Menu";
+import "./NavBar.css";
+
+function NavBar() {
+    const [showNav, setShowNav] = useState(false)
+}
+
+export default NavBar;
