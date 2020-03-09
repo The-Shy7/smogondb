d2fa3192b996db8b3e6616d21fe2e2deca6901dc
tahmit <tahmit@uw.edu>
2020-03-08 22:51:18 -0700

    changed to constructor-render

--- src/components/pokegrid/PokeGrid.js
+++ src/components/pokegrid/PokeGrid.js
@@ -1,27 +1,27 @@
 import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
-import "./pokegrid.css";
+import "./PokeGrid.css";
 import Detail from "./../detail/Detail";
 
-function PokeGrid() {
+export default class PokeGrid extends Component {
+  render() {
     return (
-        <section className="pokeGridContainer">
-          {this.props.pokemonList.map(({ name, id }) => (
-            <Link key={id} to={`/detail/${name}`}>
-              <div className="pokeGridItemContainer" key={id}>
-                <img
-                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
-                  alt="pokemon"
-                />
-                <span className="pokeName">
-                  {name} #{id}
-                </span>
-              </div>
-            </Link>
-          ))}
-          <Route exact path="detail/:name" component={Detail} />
-        </section>
-    )
+      <section className="pokeGridContainer">
+        {this.props.pokemonList.map(({ name, id }) => (
+          <Link key={id} to={`/detail/${name}`}>
+            <div className="pokeGridItemContainer" key={id}>
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+                alt="pokemon"
+              />
+              <span className="pokeName">
+                {name} #{id}
+              </span>
+            </div>
+          </Link>
+        ))}
+        <Route exact path="detail/:name" component={Detail} />
+      </section>
+    );
+  }
 }
-
-export default PokeGrid;
