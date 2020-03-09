d4e6921ffc7ce5a3de99ecca9ab34993ee83c53a
tahmit <tahmit@uw.edu>
2020-03-08 22:51:39 -0700

    changed to constructor-render

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import Detail from "./../detail/Detail";
 import { Route } from "react-router-dom";
 import Filter from "./../filter/Filter";
@@ -6,65 +6,67 @@ import { withRouter } from "react-router";
 import Search from "@material-ui/icons/Search";
 import "./SearchBar.css";
 
-function SearchBar() {
-    const [searchQuery, setSearchQuery] = useState('')
-    const [showFilters, setShowFilters] = useState(false)
-    this.handleSubmit = this.handleSubmit.bind(this)
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: "",
+      showFilters: false
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleChange = (event) => {
-        const inputValue = event.target.value;
-        setSearchQuery(inputValue)
+  handleChange = event => {
+    const inputValue = event.target.value;
+    this.setState({ searchQuery: inputValue });
 
-        if (!inputValue) {
-            return ''
-        }
+    if (!inputValue) return "";
+  };
+  handleSubmit = event => {
+    const { searchQuery } = this.state;
+    event.preventDefault();
+    const value = this.state.searchQuery;
+    this.setState({ searchQuery: "" });
+    if (value) {
+      return this.props.history.push(`/detail/${searchQuery}`);
     }
+  };
+  handleFilter = () => {
+    this.setState({ showFilters: false });
+  };
 
-    handleSubmit = (event) => {
-        const {searchQuery} = ''
-        event.preventDefault()
-        const value = searchQuery;
-        setSearchQuery('')
-        if (value) {
-            return this.props.history.push(`/detail/${searchQuery}`)
-        }
-
-    }
-
-    handleFilter = () => {
-        setShowFilters(false)
-    }
-
+  render() {
+    const { searchQuery, showFilters } = this.state;
     return (
-        <div className="searchBar">
-            <form className="searchForm" onSubmit={this.handleSubmit}>
-            <input
-                placeholder="Search Pokemon"
-                value={searchQuery}
-                onChange={this.handleChange}
-                className="inputField"
-            />
+      <div className="searchBar">
+        <form className="searchForm" onSubmit={this.handleSubmit}>
+          <input
+            placeholder="Search Pokemon"
+            value={searchQuery}
+            onChange={this.handleChange}
+            className="inputField"
+          />
 
-            <button className="submitButton">
-                <Search style={{ fontSize: 13 }} />
-            </button>
-            </form>
-            {this.props.location.pathname === "/" && (
-            <button
-                className="filterButton"
-                onClick={() => {
-                    setShowFilters(!showFilters)
-                }}
-            >
-                Filter
-            </button>
-            )}
-            {showFilters && (
-            <Filter showFilter={this.handleFilter} filter={this.props.filter} />
-            )}
-            <Route exact path="detail/:name" component={Detail} />
-        </div>
+          <button className="submitButton">
+            <Search style={{ fontSize: 13 }} />
+          </button>
+        </form>
+        {this.props.location.pathname === "/" && (
+          <button
+            className="filterButton"
+            onClick={() => {
+              this.setState({ showFilters: !showFilters });
+            }}
+          >
+            Filter
+          </button>
+        )}
+        {showFilters && (
+          <Filter showFilter={this.handleFilter} filter={this.props.filter} />
+        )}
+        <Route exact path="detail/:name" component={Detail} />
+      </div>
     );
+  }
 }
-
 export default withRouter(SearchBar);
